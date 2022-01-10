ed46fe1ae21c949304f0e2c5f1c9d734ab8e127d
Norbert Nemesh <nemeshnorbert@gmail.com>
2022-01-10 18:22:55 +0300

    Update abstract_factory code

--- creational/abstract_factory/src/main.cpp
+++ creational/abstract_factory/src/main.cpp
@@ -1,11 +1,16 @@
+#include <memory>
+
+#include "abstract_product_x.h"
+#include "abstract_product_y.h"
+#include "abstract_factory.h"
 #include "factory_a.h"
 #include "factory_b.h"
 
 int main(int argc, char** argv)
 {
-    auto factory = std::make_shared<FactoryB>();
-    auto productX = factory->CreateProductX();
-    auto productY = factory->CreateProductY();
+    std::shared_ptr<AbstractFactory> factory = std::make_shared<FactoryB>();
+    std::shared_ptr<AbstractProductX> productX = factory->CreateProductX();
+    std::shared_ptr<AbstractProductY> productY = factory->CreateProductY();
     productX->Operation();
     productY->Operation();
     return 0;
