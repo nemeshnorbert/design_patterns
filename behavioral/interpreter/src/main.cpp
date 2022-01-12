87f84b3c5f28cc84d7176aecebf2ac7f08200bff
Norbert Nemesh <nemeshnorbert@gmail.com>
2022-01-12 03:54:42 +0300

    Update interpreter code

--- behavioral/interpreter/src/main.cpp
+++ behavioral/interpreter/src/main.cpp
@@ -1,17 +1,20 @@
+#include <memory>
+
+#include "abstract_expression.h"
 #include "terminal_expression.h"
 #include "nonterminal_expression.h"
 
 int main(int argc, char** argv)
 {
-    auto nonterminal = std::make_shared<NonterminalExpression>();
+    std::shared_ptr<NonterminalExpression> nonterminal = std::make_shared<NonterminalExpression>();
     nonterminal->Add(std::make_shared<TerminalExpression>());
     nonterminal->Add(std::make_shared<TerminalExpression>());
 
-    auto start = std::make_shared<NonterminalExpression>();
+    std::shared_ptr<NonterminalExpression> start = std::make_shared<NonterminalExpression>();
     start->Add(std::make_shared<TerminalExpression>());
     start->Add(nonterminal);
 
-    auto context = Context();
+    auto context = Context{};
     start->Interpret(context);
 
     return 0;
